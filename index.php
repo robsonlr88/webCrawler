e56ae0fb895f0bc724e2961e04e035413abfae27
Robson Rocha <robson88@gmail.com>
2022-06-01 16:14:54 -0400

    init

--- index.php
+++ index.php
@@ -36,8 +36,8 @@ if($_POST["pages"]!="")
 <html>
 <head>
   <title>PHP Crawler - Agency Analytics</title>
-  <link href="../../lib/bootstrap/css/bootstrap.css" rel="stylesheet" />
-  <link href="../../lib/bootstrap/select/bootstrap-select.min.css" rel="stylesheet" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 </head>
 <body class="bg-light">
   <div class="container p-3">
@@ -111,9 +111,7 @@ if($_POST["pages"]!="")
   </div>
 
 
-  <script src="../../lib/javascript/jquery-3.1.1.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  <script src="../../lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 
 
